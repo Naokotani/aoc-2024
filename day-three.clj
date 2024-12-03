baf7e60daddaca36079d5cdfa0c8e96555449e44
Chris Hughes <naokotani@protonmail.com>
2024-12-03 14:51:28 -0400

    Day three second start done

--- day-three.clj
+++ day-three.clj
@@ -5,13 +5,24 @@
 (def input
   (slurp  "resources/day-three.txt"))
 
-input
-
-
 (defn string->list []
   (map str input))
 
-(string->list)
+(defn build-enabled [char-list]
+  (loop [list char-list enable-list (vector)]
+    (let [e (conj enable-list (first list))]
+      (cond
+        (= (first list) ")") (conj enable-list (first list))
+        (not (re-matches #"^[don't()]$" (first list))) '()
+        (empty? (rest list)) '()
+        :else (recur (rest list) e)))))
+
+(defn parse-enabled [enabled]
+  (let [e (apply str enabled)]
+    (cond
+      (re-matches #"^do\(\)$" (apply str enabled)) '(true)
+      (re-matches #"^don't\(\)$" (apply str enabled)) '(false)
+      :else '())))
 
 (defn mul? [mul]
   (re-matches #"^mul\(\d+,\d+\)$" (apply str mul)))
@@ -26,14 +37,12 @@ input
     (* (first mul) (last mul))
     0))
 
-
 (defn mul-result [mul]
   (mult-mul-list (mul-string->list (mul? mul))))
 
 (defn build-mul [char-list]
   (loop [list char-list mul-list (vector)]
     (let [m (conj mul-list (first list))]
-      (println m)
       (cond
         (= (first list) ")") (conj mul-list (first list))
         (not (re-matches #"^[mul(),0-9]$" (first list))) '()
@@ -45,22 +54,35 @@ input
     (mul-result (build-mul list))
     0))
 
+(defn parse-corrupted-string-with-do [list]
+  (loop [char-list list total 0 enabled '(true)]
+    (cond
+      (= "m" (first char-list)) (recur (rest char-list)
+                                       (if (= (first enabled) true)
+                                         (+ (parse-mul char-list) total)
+                                         total)
+                                       enabled)
+      (= "d" (first char-list)) (recur
+                                 (rest char-list)
+                                 total
+                                 (if (not-empty (parse-enabled (build-enabled char-list)))
+                                                                (parse-enabled (build-enabled char-list))
+                                                                enabled))
+      (empty? (rest char-list)) total
+      :else (recur (rest char-list) total enabled))))
+
 (defn parse-corrupted-string [list]
   (loop [char-list list total 0]
-    (println total)
     (cond
       (= "m" (first char-list)) (recur (rest char-list) (+ (parse-mul char-list) total))
       (empty? (rest char-list)) total
       :else (recur (rest char-list) total))))
 
-
 (comment
-  (mul? '("m" "u" "l" "(" "1" "," "2" ")"))
-  (mul-string->list (mul? '("m" "u" "l" "(" "1" "," "2" ")")))
-  (mult-mul-list (mul-string->list (mul? '("m" "u" "l" "(" "1" "," "2" ")"))))
-  (mul-result '("m" "u" "l" "(" "1" "," "2" ")"))
+  
+  (println (parse-corrupted-string (string->list)))
+  (println (parse-corrupted-string-with-do (string->list)))
 
-  (parse-corrupted-string (string->list))
   )
     
 
